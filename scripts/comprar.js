f4008b23e8d19ed96a0987296c4829be7dcf570b
Juan Hoyos <juanhoyos1347@gmail.com>
2022-03-30 12:13:57 -0500

    footer terminado

---
+++ scripts/comprar.js
@@ -0,0 +1,11 @@
+const btnCalcular = document.getElementById('calcularCantidad');
+const cantidadComputador = document.getElementById('cantidadComputadores');
+
+//Al recargar la pagina
+document.addEventListener('DOMContentLoaded',() => {
+
+})
+
+//Eventos
+btnCalcular.addEventListener('click',()=>{
+})
